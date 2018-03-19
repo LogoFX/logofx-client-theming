9d813406f7da48467939e021791fd6de18f55c7d
dmitrybublik <dmitry.buba@gmail.com>
2018-03-19 02:02:22 +0200

    Removed NuGet references.

--- src/LogoFX.Client.Theming/ColorTheme.cs
+++ src/LogoFX.Client.Theming/ColorTheme.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using JetBrains.Annotations;
 
 namespace LogoFX.Client.Theming
 {
     [Serializable]
     public sealed class ColorTheme
     {
-        public string Name { get; [UsedImplicitly] set; }
+        public string Name { get; set; }
 
-        public ResourceEntry[] Entries { get; [UsedImplicitly] set; }
+        public ResourceEntry[] Entries { get; set; }
     }
 }
